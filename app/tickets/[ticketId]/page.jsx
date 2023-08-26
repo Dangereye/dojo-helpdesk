312e128fffbb57615c9f601b922cd4255a2da211
Craig Puxty <craig_puxty@talktalk.net>
2023-08-26 21:24:47 +0100

    Added dynamic route [ticketId], updated TicketList.

---
+++ app/tickets/[ticketId]/page.jsx
@@ -0,0 +1,28 @@
+async function getTicket(ticketId) {
+  const res = await fetch(`http://localhost:4000/tickets/${ticketId}`, {
+    next: {
+      revalidate: 60,
+    },
+  });
+  return res.json();
+}
+
+export default async function TicketDetails({ params }) {
+  const { ticketId } = params;
+  const ticket = await getTicket(ticketId);
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className='card'>
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
+  );
+}
