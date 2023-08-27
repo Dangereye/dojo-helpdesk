b1e6edeeb7ea8739ca10ebb6afa8bf3fd9250f29
Craig Puxty <craig_puxty@talktalk.net>
2023-08-27 12:19:33 +0100

    Added Loading. Loading UI & Suspense.

--- app/tickets/[ticketId]/page.jsx
+++ app/tickets/[ticketId]/page.jsx
@@ -9,6 +9,8 @@ export async function generateStaticParams() {
 }
 
 async function getTicket(ticketId) {
+  // Imitate delay
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const res = await fetch(`http://localhost:4000/tickets/${ticketId}`, {
     next: {
       revalidate: 60,
