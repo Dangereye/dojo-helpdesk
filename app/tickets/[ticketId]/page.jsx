b306a60c80f4dc77d1dd89838eee9858ea6dd0cd
Craig Puxty <craig_puxty@talktalk.net>
2023-08-27 21:25:44 +0100

    Removed delay > TicketList & [ticketId].

--- app/tickets/[ticketId]/page.jsx
+++ app/tickets/[ticketId]/page.jsx
@@ -10,7 +10,7 @@ export async function generateStaticParams() {
 
 async function getTicket(ticketId) {
   // Imitate delay
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
   const res = await fetch(`http://localhost:4000/tickets/${ticketId}`, {
     next: {
       revalidate: 60,
