dfc6860a5c1fbc55219ee38d45d43f8252b8488e
Craig Puxty <craig_puxty@talktalk.net>
2023-08-29 20:37:43 +0100

    Added [ticketId]/route.

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -12,12 +12,12 @@ export async function POST(req) {
   const ticket = await req.json();
 
   const res = await fetch('http://localhost:4000/tickets', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(ticket),
-    });
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(ticket),
+  });
 
-    const newTicket = await res.json()
+  const newTicket = await res.json();
 
-    return NextResponse.json(newTicket,{status:201})
+  return NextResponse.json(newTicket, { status: 201 });
 }
