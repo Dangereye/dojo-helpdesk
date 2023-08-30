57ad615f6ddc3dc6d0fa805dd84a631b2fa7c32b
Craig Puxty <craig_puxty@talktalk.net>
2023-08-30 19:44:39 +0100

    Updated auth & dashboard layout to protect routes.

--- app/(auth)/verify/page.jsx
+++ app/(auth)/verify/page.jsx
@@ -2,7 +2,7 @@ export default function Verify() {
   return (
     <main className='text-center'>
       <h2>Thanks for registering!</h2>
-      <p>Before logging in, you need t verify your email address.</p>
+      <p>Before logging in, you need to verify your email address.</p>
     </main>
   );
 }
