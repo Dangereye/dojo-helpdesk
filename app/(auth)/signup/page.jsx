fc6f36ed3eb2380eb6ad53def19c8405499af225
Craig Puxty <craig_puxty@talktalk.net>
2023-08-30 15:07:59 +0100

    Added AuthForm, updated login & signup pages.

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,17 @@
+'use client';
+
+import AuthForm from '../AuthForm';
+
 export default function SignUp() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log('User sign up: ', email, password);
+  };
+
   return (
     <main>
       <h2 className='text-center'>Sign up</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
