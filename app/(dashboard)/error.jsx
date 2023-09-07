f2ec8417f05ae8a4d817704b7304e2688467aaad
Craig Puxty <craig_puxty@talktalk.net>
2023-09-07 21:02:12 +0100

    Updated error message for production build.

--- app/(dashboard)/error.jsx
+++ app/(dashboard)/error.jsx
@@ -4,7 +4,7 @@ export default function error({ error, reset }) {
   return (
     <main className='text-center'>
       <h2 className='text-4xl'>Oh No!</h2>
-      <p>{error.message}</p>
+      <p>Something went wrong.</p>
       <button onClick={reset} className='btn-primary mx-auto my-4'>
         Maybe try again?
       </button>
